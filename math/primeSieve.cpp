0fb5b77d3faf56b8c1d83516256f359086f3addd
kittobi1992 <Tobias.Heuer@gmx.net>
2014-11-21 16:02:51 +0100

    Primzahlsieb
    Signed-off-by: kittobi1992 <Tobias.Heuer@gmx.net>

---
+++ math/primeSieve.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+vector<int> primeSieve(int n) {
+	vector<int> primes;
+	vector<bool> isPrime(n,true);
+	for(int i = 2; i < n; i+=2) {
+		if(i*i <= n) {
+			if(isPrime[i]) {
+				primes.push_back(i);
+				for(int j = 1; i*j < n; j++) {
+					isPrime[i*j] = false;
+				}
+			}
+		}
+		else {
+			if(isPrime[i])
+				primes.push_back(i);
+		}
+		if(i == 2)
+			i--;
+	}
+	return primes;
+} 
