4cc304e57566d149582d974cdaf4a7f724c6b5c1
pjungeblut <paul.jungeblut@gmail.com>
2014-11-23 22:59:10 +0100

    Floyd Warshall

---
+++ graph/floydWarshall.cpp
@@ -0,0 +1,8 @@
+//initialize adjmat, adjmat[i][i] = 0, adjmat[i][j] = INF if no edge is between i and j
+for (k = 0; k < MAX_V; k++) {
+	for (i = 0; i < MAX_V; i++) {
+		for (j = 0; j < MAX_V; j++) {
+			if (adjmat[i][k] + adjmat[k][j] < adjmat[i][j]) adjmat[i][j] = adjmat[i][k] + adjmat[k][j];
+		}
+	}
+}
