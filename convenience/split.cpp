1199b187daa65b762d2f6b681d54e9aac2780783
pjungeblut <paul.jungeblut@gmail.com>
2014-11-25 20:57:41 +0100

    Eingabehilfe für Zeilenweise-Eingabe

---
+++ convenience/split.cpp
@@ -0,0 +1,9 @@
+vector<string> split(string &s, string delim) { //zerlegt s anhand aller Zeichen in delim
+	vector<string> result; char *token;
+	token = strtok((char*)s.c_str(), (char*)delim.c_str());
+	while (token != NULL) {
+		result.push_back(string(token));
+		token = strtok(NULL, (char*)delim.c_str());
+	}
+	return result;
+}
