9cf344075f66a3bd2e404b08b72812356bb3f565
Paul Jungeblut <s_jungeb@i08pc71.atis-stud.uni-karlsruhe.de>
2014-11-26 18:34:33 +0100

    Feinschliff

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 //Preprocessing Substring sub for KMP-Search
 vector<int> kmp_preprocessing(string& sub) {
 	vector<int> b(sub.size() + 1);
