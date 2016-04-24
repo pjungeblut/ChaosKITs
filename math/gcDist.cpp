86978d60f8d2148362119fdccf680320a50dcc1f
Paul Jungeblut <paul.jungeblut@gmail.com>
2016-04-24 10:46:02 +0200

    Adding 3D spheres and some small changes to KMP.

---
+++ math/gcDist.cpp
@@ -0,0 +1,23 @@
+// Great Cirlce Distance mit Längen- und Breitengrad.
+double gcDist(double pLat, double pLon, double qLat, double qLon, double radius) {
+  pLat *= PI / 180; pLon *= PI / 180; qLat *= PI / 180; qLon *= PI / 180;
+  return radius * acos(cos(pLat) * cos(pLon) * cos(qLat) * cos(qLon) +
+                       cos(pLat) * sin(pLon) * cos(qLat) * sin(qLon) +
+                       sin(pLat) * sin(qLat));
+}
+
+// Great Cirlce Distance mit kartesischen Koordinaten.
+double gcDist(point p, point q) {
+  return acos(p.x * q.x + p.y * q.y + p.z * q.z);
+}
+
+// 3D Punkt in kartesischen Koordinaten.
+struct point{
+  double x, y, z;
+  point() {}
+  point(double x, double y, double z) : x(x), y(y), z(z) {}
+  point(double lat, double lon) {
+    lat *= PI / 180.0; lon *= PI / 180.0;
+    x = cos(lat) * sin(lon); y = cos(lat) * cos(lon); z = sin(lat);
+  }
+};
--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,30 +1,27 @@
-//Preprocessing Substring sub for KMP-Search
-vector<int> kmp_preprocessing(string& sub) {
-	vector<int> b(sub.size() + 1);
-	b[0] = -1;
-	int i = 0, j = -1;
-	while(i < sub.size()) {
-		while(j >= 0 && sub[i] != sub[j])
-			j = b[j];
-		i++; j++;
-		b[i] = j;
-	}
-	return b;
+// Laufzeit: O(n + m), n = #Text, m = #Pattern
+vector<int> kmp_preprocessing(string &sub) {
+  vector<int> b(sub.length() + 1);
+  b[0] = -1;
+  int i = 0, j = -1;
+  while (i < (int)sub.length()) {
+    while (j >= 0 && sub[i] != sub[j]) j = b[j];
+    i++; j++;
+    b[i] = j;
+  }
+  return b;
 }
 
-//Searching after Substring sub in s
-vector<int> kmp_search(string& s, string& sub) {
-	vector<int> pre = kmp_preprocessing(sub);
-	vector<int> result;
-	int i = 0, j = -1;
-	while(i < s.size()) {
-		while(j >= 0 && s[i] != sub[j])
-			j = pre[j];
-		i++; j++;
-		if(j == sub.size()) {
-			result.push_back(i-j);
-			j = pre[j];
-		}
-	}
-	return result;
+vector<int> kmp_search(string &s, string &sub) {
+  vector<int> pre = kmp_preprocessing(sub);
+  vector<int> result;
+  int i = 0, j = 0;
+  while (i < (int)s.length()) {
+    while (j >= 0 && s[i] != sub[j]) j = pre[j];
+    i++; j++;
+    if (j == (int)sub.length()) {
+      result.push_back(i - j);
+      j = pre[j];
+    }
+  }
+  return result;
 }
