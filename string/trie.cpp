2cb378d70cd847491680e7768798169ce3f93f00
Paul Jungeblut <paul.jungeblut@gmail.com>
2016-10-05 22:23:54 +0200

    Improve trie code.

--- string/trie.cpp
+++ string/trie.cpp
@@ -5,21 +5,18 @@ struct node {
 	node() { for(int i = 0; i < 26; i++) e[i] = NULL; }	
 };
 
-void insert(node *root, string *txt, int s) {
-	if(s == txt->length()) root->c++;
+void insert(node *root, string &txt, int s) { // Laufzeit: O(|txt|)
+	if(s == (int)txt.size()) root->c++;
 	else {
-		int idx = (int)(*txt[s] - 'a');
-		if(root->e[idx] == NULL) {
-			root->e[idx] = new node();
-		}
+		int idx = (int)(txt[s] - 'a');
+		if(root->e[idx] == NULL) root->e[idx] = new node();
 		insert(root->e[idx], txt, s+1);
 	}
 }
 
-int contains(node *root, string *txt, int s) {
-	if(s == txt->length()) return root->c;
-	int idx = (int)(*txt[s] - 'a');
-	if(root->e[idx] != NULL) {
-			return contains(root->e[idx], txt, s+1);
-	} else return 0;
+int contains(node *root, string &txt, int s) { // Laufzeit: O(|txt|)
+	if(s == txt.size()) return root->c;
+	int idx = (int)(txt[s] - 'a');
+	if(root->e[idx] != NULL) return contains(root->e[idx], txt, s + 1);
+	else return 0;
 }
