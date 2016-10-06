9d121e24674cc1355d27ff35688a205766333928
Paul Jungeblut <paul.jungeblut@gmail.com>
2016-10-06 19:53:09 +0200

    Bellmann Ford now with C++11.

--- graph/bellmannFord.cpp
+++ graph/bellmannFord.cpp
@@ -1,10 +1,4 @@
 // Laufzeit: O(|V|*|E|)
-struct edge {
-	int from; int to; int cost;
-	edge () {};
-	edge (int from, int to, int cost) : from(from), to(to), cost(cost) {};
-};
-
 vector<edge> edges; // Kanten einfügen!
 vector<int> dist, parent;
 
@@ -12,19 +6,15 @@ void bellmannFord() {
 	dist.assign(NUM_VERTICES, INF); dist[0] = 0;
 	parent.assign(NUM_VERTICES, -1);
 	for (int i = 0; i < NUM_VERTICES - 1; i++) {
-		for (int j = 0; j < (int)edges.size(); j++) {
-			if (dist[edges[j].from] + edges[j].cost < dist[edges[j].to]) {
-				dist[edges[j].to] = dist[edges[j].from] + edges[j].cost;
-				parent[edges[j].to] = edges[j].from;
-			}
-		}
-	}
+		for (auto &e : edges) {
+			if (dist[e.from] + e.cost < dist[e.to]) {
+				dist[e.to] = dist[e.from] + e.cost;
+				parent[e.to] = e.from;
+	}}}
 
 	// "dist" und "parent" sind korrekte kürzeste Pfade.
 	// Folgende Zeilen prüfen nur negative Kreise.
-	for (int j = 0; j < (int)edges.size(); j++) {
-		if (dist[edges[j].from] + edges[j].cost < dist[edges[j].to]) {
+	for (auto &e : edges) {
+		if (dist[e.from] + e.cost < dist[e.to]) {
 			// Negativer Kreis gefunden.
-		}
-	}
-}
+}}}
