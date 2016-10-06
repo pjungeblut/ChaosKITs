7cf8f4e10ac917ed8b900e43e294e26200329ee8
Paul Jungeblut <paul.jungeblut@gmail.com>
2016-10-06 19:44:29 +0200

    Saving space in Dijkstra.

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -2,22 +2,16 @@
 void dijkstra(int start) {
 	priority_queue<ii, vector<ii>, greater<ii> > pq;
 	vector<int> dist(NUM_VERTICES, INF), parent(NUM_VERTICES, -1);
-
-	dist[start] = 0;
-	pq.push(ii(0, start));
+	dist[start] = 0; pq.push(ii(0, start));
 
 	while (!pq.empty()) {
 		ii front = pq.top(); pq.pop();
 		int curNode = front.second, curDist = front.first;
-		
-		if (curDist > dist[curNode]) continue;
+		if (curDist > dist[curNode]) continue; // WICHTIG!
 		
 		for (auto n : adjlist[curNode]) {
 			int nextNode = n.first, nextDist = curDist + n.second;
 			if (nextDist < dist[nextNode]) {
 				dist[nextNode] = nextDist; parent[nextNode] = curNode;
 				pq.push(ii(nextDist, nextNode));
-			}
-		}
-	}
-}
+}}}}
