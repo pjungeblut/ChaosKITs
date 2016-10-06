461ed24a1542a59e999807c49a4ca05abb6b414a
Paul Jungeblut <paul.jungeblut@gmail.com>
2016-10-06 20:00:50 +0200

    Floyd Warshall improvements.

--- graph/floydWarshall.cpp
+++ graph/floydWarshall.cpp
@@ -1,13 +1,9 @@
-// Laufzeit: O(|V|^3)
-// Initialisiere mat: mat[i][i] = 0, mat[i][j] = INF falls i & j nicht adjazent, Länge sonst.
+// Initialisiere mat: mat[i][i] = 0, mat[i][j] = INF falls i & j nicht
+// adjazent, Länge sonst. Laufzeit: O(|V|^3)
 void floydWarshall() {
 	for (k = 0; k < MAX_V; k++) {
 		for (i = 0; i < MAX_V; i++) {
 			for (j = 0; j < MAX_V; j++) {
 				if (mat[i][k] != INF && mat[k][j] != INF && mat[i][k] + mat[k][j] < mat[i][j]) {
 					mat[i][j] = mat[i][k] + mat[k][j];
-				}
-			}
-		}
-	}
-}
+}}}}}
