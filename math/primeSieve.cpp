ec035ec7395db153834b8ba96b2ce54b597483b0
Paul Jungeblut <paul.jungeblut@gmail.com>
2016-11-09 20:39:31 +0100

    Adding Kirchhoffs Theorem.

--- math/primeSieve.cpp
+++ math/primeSieve.cpp
@@ -1,19 +1,19 @@
 // Laufzeit: O(n * log log n)
-#define N 100000001 // Bis 10^8 in unter 64MB Speicher.
-bitset<N / 2> isPrime;
+#define N 100000000 // Bis 10^8 in unter 64MB Speicher.
+bitset<N / 2> isNotPrime;
 
 inline bool check(int x) { // Diese Methode zum Lookup verwenden.
   if (x < 2) return false;
   else if (x == 2) return true;
   else if (!(x & 1)) return false;
-  else return !isPrime[x / 2];
+  else return !isNotPrime[x / 2];
 }
 
-inline int primeSieve(int n) { // Rückgabe: Anzahl der Primzahlen <= n.
-  int counter = 1;
-  for (int i = 3; i <= min(N, n); i += 2) {
-    if (!isPrime[i / 2]) {
-      for (int j = 3 * i; j <= min(N, n); j+= 2 * i) isPrime[j / 2] = 1;
+inline int primeSieve() { // Rückgabe: Anzahl der Primzahlen < N.
+  int counter = 1; // Die 2, die sonst vergessen würde.
+  for (int i = 3; i < N; i += 2) {
+    if (!isNotPrime[i / 2]) {
+      for (int j = 3 * i; j < N; j+= 2 * i) isNotPrime[j / 2] = 1;
       counter++;
   }}
   return counter;
