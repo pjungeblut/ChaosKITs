0d90b5cda91a7a9145f6f70d13994090ae6dc28b
Paul Jungeblut <paul.jungeblut@gmail.com>
2017-10-22 12:55:53 +0200

    Adding function to count the number of 1-bits in an integer.

--- other/bitOps.cpp
+++ other/bitOps.cpp
@@ -14,3 +14,9 @@ a = -1
 a = (1 << n) - 1
 // Iteriert über alle Teilmengen einer Bitmaske (außer der leeren Menge).
 for (int subset = bitmask; subset > 0; subset = (subset - 1) & bitmask)
+// Zählt Anzahl der gesetzten Bits.
+int numberOfSetBits(int i) {
+  i = i - ((i >> 1) & 0x55555555);
+  i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+  return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+}
