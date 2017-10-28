5e3d0a8cc013c4461a1e096604a9d67b8f3e0d8e
Paul Jungeblut <paul.jungeblut@gmail.com>
2017-10-28 11:50:29 +0200

    Removing Kruskal code.

--- graph/kruskal.cpp
+++
@@ -1,9 +0,0 @@
-// Union-Find Implementierung von oben. Laufzeit: O(|E|*log(|E|))
-sort(edges.begin(), edges.end());
-vector<ii> mst; int cost = 0;
-for (auto &e : edges) {
-  if (findSet(e.from) != findSet(e.to)) {
-    unionSets(e.from, e.to);
-    mst.push_back(ii(e.from, e.to));
-    cost += e.cost;
-}}
