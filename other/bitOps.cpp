00b7570f067b4261c05094b2e80dfb6c4c8c1742
Paul Jungeblut <paul.jungeblut@gmail.com>
2017-10-28 12:03:10 +0200

    Adding builtins to count number of set bits.

--- other/bitOps.cpp
+++ other/bitOps.cpp
@@ -15,8 +15,5 @@ a = (1 << n) - 1
 // Iteriert über alle Teilmengen einer Bitmaske (außer der leeren Menge).
 for (int subset = bitmask; subset > 0; subset = (subset - 1) & bitmask)
 // Zählt Anzahl der gesetzten Bits.
-int numberOfSetBits(int i) {
-  i = i - ((i >> 1) & 0x55555555);
-  i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-  return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
-}
+int __builtin_popcount(unsigned int x);
+int __builtin_popcountll(unsigned long long x);
