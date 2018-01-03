ad25c63e9eb95cea1dab855bfc93ce7ec0754833
Paul Jungeblut <paul.jungeblut@gmail.com>
2018-01-03 22:08:37 +0100

    Removing whitespace in gcd.

--- math/gcd-lcm.cpp
+++ math/gcd-lcm.cpp
@@ -1,3 +1,3 @@
 // Laufzeiten: O(log(a) + log(b))
-ll gcd(ll a, ll b) { return b == 0 ? a : gcd (b, a % b); }
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
 ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
